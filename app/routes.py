d2db98921dce24ce57e8eb2415ae3481da65f301
SiddAjriY2Jaccount <y2jsiddajri@gmail.com>
2020-05-15 01:24:57 +0530

    updated routes, index

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Sup!"
+def index():	
+    user = {'username': 'Sidd_Ajri'}    #mock user object
+    return render_template('index.html', title='Ahoy Maties', user=user) #rendering your HTML
